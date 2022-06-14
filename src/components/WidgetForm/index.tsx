22ba830734a2e2c96a3034c4f429b85a25361331
Italo Souza Melo <italosm29@gmail.com>
2022-06-14 18:19:07 -0300

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,79 @@
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+    placeholder:
+      "Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...",
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+    placeholder:
+      "Teve uma ideia de melhoria ou de nova funcionalidade? Conta pra gente!",
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+    placeholder: "Queremos te ouvir. O que você gostaria de nos dizer?",
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequest={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por{" "}
+        <a
+          target="_blank"
+          className="underline underline-offset-2"
+          href="https://www.linkedin.com/in/italo-smelo/"
+        >
+          Italo Melo
+        </a>
+        .
+      </footer>
+    </div>
+  );
+}
